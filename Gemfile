5cd38410636d2c762a8bc0b4ffd7a97713bd8bda
Wusiman Yibulayin <wusiman.yibulayin01@stclairconnect.ca>
2022-04-17 23:07:48 -0400

    Installed ruby 2.6.6 with Gemfile"

--- Gemfile
+++ Gemfile
@@ -4,6 +4,10 @@ git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
+
+ruby '2.6.6'
+
+
 gem 'rails', '~> 5.1.4'
 
 gem 'faker'
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20220418001214) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.text "title"
     t.text "content"
